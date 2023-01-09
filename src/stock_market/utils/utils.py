82cfd4906def09111c2cb2ec8ac458025a8c700c
Tino Michael <tino.michael.87@gmail.com>
2023-01-09 11:35:05 +0100

    skip actions:
    action column now parameter
    actions are parsed with `contains` instead of `starts_with`

--- src/stock_market/utils/utils.py
+++ src/stock_market/utils/utils.py
@@ -1,11 +1,11 @@
-def skip_actions(df, actions):
+def skip_actions(df, actions, action_column="Action"):
     """
     Removes rows from the data frame where the "Action" entry starts with anything given by the
     `actions` list.
     """
 
     for skip in actions:
-        df = df[df.loc[:, "Action"].str.startswith(skip)==False]
+        df = df[df.loc[:, action_column].str.contains(skip)==False]
 
     return df
 
@@ -16,11 +16,11 @@ def get_owned_shares(
         action_column="Action", n_shares_column="Shares"):
     """
     Returns the number of shares owned from the given data frame.
-    The strings in `buy_actions` and `sell_actions` are search for in the `action_column` and 
+    The strings in `buy_actions` and `sell_actions` are searched for in the `action_column` and
     define which entries are to be considered as shares bought and sold.
     """
 
-    bought = sum(df[df.loc[:, action_column].str.startswith(a)].loc[:, n_shares_column].sum() for a in buy_actions)
-    sold = sum(df[df.loc[:, action_column].str.startswith(a)].loc[:, n_shares_column].sum() for a in sell_actions)
+    bought = sum(df[df.loc[:, action_column].str.contains(a)].loc[:, n_shares_column].sum() for a in buy_actions)
+    sold = sum(df[df.loc[:, action_column].str.contains(a)].loc[:, n_shares_column].sum() for a in sell_actions)
 
     return bought - sold
